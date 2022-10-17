4b15f3031dcdc13b667294abad620da0320ad47e
Johnnykeyz <adebulejohnfav@gmail.com>
2022-10-17 16:46:46 -0700

    John 4th commit

---
+++ print_cent.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_cent - function that prints the modulo charcter to stdout
+ * @args: variadic parameter
+ *
+ * Return: the number of characters printed to stdout
+ */
+int print_cent(va_list args)
+{
+	char percent;
+	int count;
+	(void)args;
+	percent = '%';
+	count = 0;
+
+	count = write(1, &percent, 1);
+
+	return (count);
+}
